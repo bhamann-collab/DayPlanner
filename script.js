1f64ff3272dd817f98eab202f18435008e17b7cd
bhamann-collab <brockohamann@gmail.com>
2020-03-24 09:04:37 +1030

    More functionality completed

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var changeText = false;
 var currentTimeBlock;
 
+toggleButtonOptions();
+
 $(".time-slot").on("click", function() {
     if(changeText === false) {
+        //Getting rid of the display rectangle
         $(this).find(".rec").css("display", "none");
+        //Inserting an input rectangle
         $(this).append($("<input type='search'>"));
+
+        $(this).find('input').val(`${$(this).find(".rec").text()}`)
+        $(this).find('input').select()
+
+        toggleButtonOptions();
 
         $(".time-slot").css("transition", "none")
         $(".time-slot").css("transform", "none")
@@ -17,11 +26,21 @@ $(".change-option-button").on("click", function() {
     //making the rectangle display visible
     currentTimeBlock.find(".rec").css("display", "inherit")
 
-    //The display rectangle getting the value of the input
-    currentTimeBlock.find("p").replaceWith(`<p>${currentTimeBlock.find("input").val()}</p>`)
-    //Removing the input
-    currentTimeBlock.find("input").remove()
+    if($(this).attr('id') === "save-button") {
+        //The display rectangle getting the value of the input
+        currentTimeBlock.find("p").replaceWith(`<p>${currentTimeBlock.find("input").val()}</p>`)
+        //Removing the input
+        currentTimeBlock.find("input").remove()
+    } else if($(this).attr('id') === "delete-button") {
+        //deleting content from .rec
+        currentTimeBlock.find("p").replaceWith(`<p></p>`)
+        currentTimeBlock.find("input").remove()
+    } else if($(this).attr('id') === "cancel-button") {
+        currentTimeBlock.find("input").remove()
+    }
     
+    toggleButtonOptions();
+
     //Restoring the hovering functionality
     $(".time-slot").css("transition", "all .2s ease-in-out")
     $(".time-slot").hover(function() {
@@ -33,3 +52,8 @@ $(".change-option-button").on("click", function() {
 
     
 })
+
+
+function toggleButtonOptions() {
+    $(".change-options").toggle("show")
+}
