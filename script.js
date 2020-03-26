022a611e27d8d8301468ef478c042c5b4b658d66
bhamann-collab <brockohamann@gmail.com>
2020-03-26 16:56:31 +1030

    the hovering functionality for the date component have been completed

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var currentTimeBlock;
 
 toggleButtonOptions();
 
+$(".day-of-year").children()[1].innerHTML = `${getTheDay()}, ${getTheDate()} ${getTheMonth()}, ${getTheYear()}`
+
 $(".time-slot").on("click", function() {
     if(changeText === false) {
         //Getting rid of the display rectangle
@@ -14,9 +16,8 @@ $(".time-slot").on("click", function() {
         $(this).find('input').select()
 
         toggleButtonOptions();
+        timeslotAnimationOff();
 
-        $(".time-slot").css("transition", "none")
-        $(".time-slot").css("transform", "none")
         changeText = true;
         currentTimeBlock = $(this)
     }
@@ -26,13 +27,12 @@ $(".change-option-button").on("click", function() {
     //making the rectangle display visible
     currentTimeBlock.find(".rec").css("display", "inherit")
 
+
+        //Did you click on the Save, Delete or Cancel button
     if($(this).attr('id') === "save-button") {
-        //The display rectangle getting the value of the input
         currentTimeBlock.find("p").replaceWith(`<p>${currentTimeBlock.find("input").val()}</p>`)
-        //Removing the input
         currentTimeBlock.find("input").remove()
     } else if($(this).attr('id') === "delete-button") {
-        //deleting content from .rec
         currentTimeBlock.find("p").replaceWith(`<p></p>`)
         currentTimeBlock.find("input").remove()
     } else if($(this).attr('id') === "cancel-button") {
@@ -42,12 +42,7 @@ $(".change-option-button").on("click", function() {
     toggleButtonOptions();
 
     //Restoring the hovering functionality
-    $(".time-slot").css("transition", "all .2s ease-in-out")
-    $(".time-slot").hover(function() {
-        $(this).css("transform", "scale(1.1)") 
-        $(this).css("cursor", "pointer") }, function() {
-        $(this).css("transform", "scale(1)")
-    })
+    timeslotAnimationOn();
     changeText = false;
 
     
@@ -57,3 +52,46 @@ $(".change-option-button").on("click", function() {
 function toggleButtonOptions() {
     $(".change-options").toggle("show")
 }
+
+function timeslotAnimationOff() {
+    $(".time-slot").css("transition", "none")
+    $(".time-slot").css("transform", "none")
+}
+
+function timeslotAnimationOn() {
+    $(".time-slot").css("transition", "all .2s ease-in-out")
+    $(".time-slot").hover(function() {
+        $(this).css("transform", "scale(1.1)") 
+        $(this).css("cursor", "pointer") }, function() {
+        $(this).css("transform", "scale(1)")
+    })
+}
+
+function getTheDay() {
+    var date = new Date()
+    var day = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"]
+    return day[date.getDay()]
+}
+
+function getTheMonth() {
+    var date = new Date()
+    var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    return month[date.getMonth()]
+}
+
+function getTheYear() {
+    var date = new Date()
+    return (date.getYear() + 1900)
+}
+
+function getTheDate() {
+    var date = new Date()
+    return date.getDate()
+}
+
+function getTheHour() {
+    var date = new Date()
+    return date.getHours()
+}
+
+
